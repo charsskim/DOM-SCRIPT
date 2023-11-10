26bd23a5b0ee0553993310ebae06413f53612aa3
charsskim <mcprince73@naver.com>
2023-11-10 12:26:03 +0900

    돔탐색

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log('main');
+const section = document.querySelector('section');
+const articles = section.querySelectorAll('article'); //유사배열
+const btn = section.querySelector('.btn');
+const childrenEl = section.children;
+const parentEl = btn.parentElement;
+const grandParentEl = btn.closest('section');
+const prevEl = btn.previousElementSibling;
+const nextEl = btn.nextElementSibling;
+const siblings = btn.parentElement.children;
+console.log(siblings);
+
+console.log('article');
+console.log(btn);
+console.log(childrenEl);
+console.log(parentEl);
+console.log(prevEl);
+console.log(nextEl);
